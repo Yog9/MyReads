952fddf906b2ff31b5ef0463f5000871dd668f9d
Yog9 <veryogita@gmail.com>
2020-04-29 09:45:12 +0530

    Updates

---
+++ precache-manifest.d087050c75669e0a668cd54247f38863.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79cc3047324f1cfcf2294e69d4ce9ff",
+    "url": "/MyReads/index.html"
+  },
+  {
+    "revision": "8d5648724a612e85db54",
+    "url": "/MyReads/static/css/main.7c4ebc9d.chunk.css"
+  },
+  {
+    "revision": "cc55356d032fa184c6f4",
+    "url": "/MyReads/static/js/2.059e7089.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyReads/static/js/2.059e7089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5648724a612e85db54",
+    "url": "/MyReads/static/js/main.406cdc75.chunk.js"
+  },
+  {
+    "revision": "2f0f9cb4692705b4b5a9",
+    "url": "/MyReads/static/js/runtime-main.464466d7.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
